d72ca61f45cf5052b4c46e57c4a051c087274685
Christopher Cartwright <chris.cartwright@dei.ca>
2016-03-03 12:31:41 -0500

    Added log4net

--- TimeTrackers/App.xaml.cs
+++ TimeTrackers/App.xaml.cs
@@ -1,54 +1,75 @@
 ﻿using System;
 using System.Threading;
 using System.Windows;
+using log4net;
+using log4net.Appender;
+using log4net.Repository;
 
 namespace TimeTrackers {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App {
-		private static readonly Mutex Mutex = new Mutex(true, "{36513326-FA2D-4005-A042-26B1DF37EAFB}");
-		private static readonly EventWaitHandle ShowApp = new EventWaitHandle(false, EventResetMode.AutoReset, "{3439C7F4-CABA-4528-9ABC-9DDC6FDEACCE}");
-		private static volatile bool _exitForegroundListener;
-		private static MainWindow _mainWindow;
-		private static Thread _foregroundListenerThread;
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+        private static readonly Mutex Mutex = new Mutex(true, "{36513326-FA2D-4005-A042-26B1DF37EAFB}");
+        private static readonly EventWaitHandle ShowApp = new EventWaitHandle(false, EventResetMode.AutoReset, "{3439C7F4-CABA-4528-9ABC-9DDC6FDEACCE}");
+        private static volatile bool _exitForegroundListener;
+        private static MainWindow _mainWindow;
+        private static Thread _foregroundListenerThread;
 
-		private static void ForegroundListener() {
-			while (!_exitForegroundListener) {
-				if (ShowApp.WaitOne(100)) {
-					Current.Dispatcher.Invoke(() => _mainWindow.WindowState = WindowState.Normal);
-				}
-			}
-		}
+        private readonly ILog _logger = LogManager.GetLogger(typeof(App));
 
-		public App() {
-			InitializeComponent();
-		}
+        private static void ForegroundListener() {
+            while (!_exitForegroundListener) {
+                if (ShowApp.WaitOne(100)) {
+                    Current.Dispatcher.Invoke(() => _mainWindow.WindowState = WindowState.Normal);
+                }
+            }
+        }
 
-		protected override void OnExit(ExitEventArgs e) {
-			base.OnExit(e);
+        public void FlushBuffers() {
+            ILoggerRepository rep = LogManager.GetRepository();
+            foreach (IAppender appender in rep.GetAppenders()) {
+                (appender as BufferingAppenderSkeleton)?.Flush();
+            }
+        }
 
-			_exitForegroundListener = true;
-			if (!_foregroundListenerThread.Join(1000)) {
-				_foregroundListenerThread.Abort();
-			}
-		}
+        public App() {
+            InitializeComponent();
 
-		[STAThread]
-		public static void Main() {
-			if (!Mutex.WaitOne(TimeSpan.Zero, true)) {
-				ShowApp.Set();
-				return;
-			}
+            _logger.Debug("Application started");
 
-			App app = new App();
-			_mainWindow = new MainWindow();
+            DispatcherUnhandledException += (sender, args) => {
+                _logger.Fatal(args.Exception);
+                FlushBuffers();
+            };
+        }
 
-			_foregroundListenerThread = new Thread(ForegroundListener);
-			_foregroundListenerThread.Start();
+        protected override void OnExit(ExitEventArgs e) {
+            base.OnExit(e);
 
-			app.Run(_mainWindow);
-			Mutex.ReleaseMutex();
-		}
-	}
+            _exitForegroundListener = true;
+            if (!_foregroundListenerThread.Join(1000)) {
+                _foregroundListenerThread.Abort();
+            }
+        }
+
+        [STAThread]
+        public static void Main() {
+            if (!Mutex.WaitOne(TimeSpan.Zero, true)) {
+                ShowApp.Set();
+                return;
+            }
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            App app = new App();
+            _mainWindow = new MainWindow();
+
+            _foregroundListenerThread = new Thread(ForegroundListener);
+            _foregroundListenerThread.Start();
+
+            app.Run(_mainWindow);
+            Mutex.ReleaseMutex();
+        }
+    }
 }
